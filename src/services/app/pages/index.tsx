7f41b37080f0acccfcd5967a1f1eea8fba5d48de
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-06 15:06:34 +0200

    fix: remove div

--- src/services/app/pages/index.tsx
+++ src/services/app/pages/index.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@fredrikkadolfsson/ui';
 
 const Index = (): JSX.Element => (
-  <div>
+  <>
     <p>Hello World!</p>
     <Button color="primary" variant="contained">
       Hej
     </Button>
-  </div>
+  </>
 );
 
 export default Index;
