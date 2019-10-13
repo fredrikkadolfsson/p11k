edadda54bf1b1c0cbe3e634c956e57104697a1bd
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-13 14:26:06 +0200

    chore: add parserOptions.project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: '@fredrikkadolfsson/eslint-config-p11k',
+  parserOptions: {
+    project: ['./src/packages/*/tsconfig.json', './src/services/*/tsconfig.json'],
+  },
 };
