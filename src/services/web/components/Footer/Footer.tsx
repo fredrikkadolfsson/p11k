5e1052f836ddffe2c5997dab5c0fe06911d9cef7
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-13 22:22:14 +0200

    fix: updated links in footer

--- src/services/web/components/Footer/Footer.tsx
+++ src/services/web/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Footer as UiFooter } from '@fredrikkadolfsson/ui';
 
-const linkSections = [{ links: [{ text: 'Start', url: '/' }, { text: 'Om oss', url: '/about' }], title: 'Något' }];
+const linkSections = [
+  {
+    links: [{ text: 'Start', url: '/' }, { text: 'Logga in', url: '/login' }, { text: 'Om oss', url: '/about' }],
+    title: 'Snabblänkar',
+  },
+];
 
 const Footer = (): JSX.Element => <UiFooter siteName="Perfect Stack" linkSections={linkSections} />;
 
