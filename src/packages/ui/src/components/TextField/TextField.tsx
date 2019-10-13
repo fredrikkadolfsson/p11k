a3a9bf1e3375bab9cbf1a3e4ab0ec974f3f336a6
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-13 23:16:54 +0200

    fix: styling on TextField

--- src/packages/ui/src/components/TextField/TextField.tsx
+++ src/packages/ui/src/components/TextField/TextField.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { default as MuiTextField, TextFieldProps } from '@material-ui/core/TextField';
+import styled from '../../lib';
 
-const TextField = (props: TextFieldProps): JSX.Element => <MuiTextField variant={'outlined' as any} {...props} />; // eslint-disable-line @typescript-eslint/no-explicit-any
+// https://stackoverflow.com/questions/58311182/type-instantiation-is-excessively-deep-and-possibly-infinite-with-emotion-styled
+const TemproraryWrapper = styled.div`
+  margin: 10px;
+`;
+
+const TextField = (props: TextFieldProps): JSX.Element => (
+  <TemproraryWrapper>
+    {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+    <MuiTextField variant={'outlined' as any} {...props} />{' '}
+  </TemproraryWrapper>
+);
 
 export default TextField;
