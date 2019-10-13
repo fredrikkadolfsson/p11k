09f112a0b580d59fa7ef1ee221c0ad594fdf7f80
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-13 22:22:35 +0200

    fix: styled login form

--- src/services/web/components/LoginForm/LoginForm.tsx
+++ src/services/web/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { Field, Form, Formik, FormikProps } from 'formik';
-import { Button, TextField } from '@fredrikkadolfsson/ui';
+import styled, { Button, TextField } from '@fredrikkadolfsson/ui';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 import { Authenticate, AuthenticateVariables } from './types/authenticate';
@@ -10,31 +10,36 @@ const AUTHENTICATE_USER = gql`
   }
 `;
 
+const StyledForm = styled(Form)`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+`;
+
 const LoginForm = (): JSX.Element => {
   const [authenticate] = useMutation<Authenticate, AuthenticateVariables>(AUTHENTICATE_USER);
 
   return (
-    <>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={async (variables, { setSubmitting }): Promise<void> => {
-          await authenticate({
-            variables: variables,
-          });
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }: FormikProps<{}>): JSX.Element => (
-          <Form>
-            <Field name="email" type="email" component={TextField} />
-            <Field name="password" type="password" component={TextField} />
-            <Button type="submit" disabled={isSubmitting}>
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={async (variables, { setSubmitting }): Promise<void> => {
+        await authenticate({
+          variables: variables,
+        });
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting }: FormikProps<{}>): JSX.Element => (
+        <StyledForm>
+          <Field name="email" type="email" label="Email" component={TextField} />
+          <Field name="password" type="password" label="Password" component={TextField} />
+          <Button type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </StyledForm>
+      )}
+    </Formik>
   );
 };
 
