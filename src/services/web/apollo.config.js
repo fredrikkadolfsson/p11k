c020bf8e9a76d03bf8abb1ded26eb5d15bf61d80
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-13 22:10:47 +0200

    fix: generation of schema

--- src/services/web/apollo.config.js
+++ src/services/web/apollo.config.js
@@ -2,9 +2,8 @@ module.exports = {
   client: {
     includes: ['**'],
     service: {
-      localSchemaFile: './schema.json',
       name: 'p11k',
-      url: 'http://localhost:4000',
+      url: 'http://localhost:4000/graphql',
     },
   },
 };
