16cd48fc8dc5507212cdf0127849e376f7fe3514
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-10-26 21:13:19 +0200

    chore: refactor config files

--- src/services/graphql/src/config.ts
+++ src/services/graphql/src/config.ts
@@ -1,6 +1,16 @@
-export default {
-  ENABLE_PLAYGROUND: true,
-  ENABLE_TRACING: true,
-  JWT_COOKIE_NAME: 'AUTHENTICATION_TOKEN',
-  PORT: 4000,
+/* eslint-disable prettier/prettier */
+type configType = {
+  ENABLE_PLAYGROUND: boolean;
+  ENABLE_TRACING: boolean;
+  JWT_COOKIE_NAME: string;
+  PORT: string;
 };
+
+const config: configType = {
+  ENABLE_PLAYGROUND: Boolean(process.env.ENABLE_PLAYGROUND ?? true),
+  ENABLE_TRACING: Boolean(process.env.ENABLE_TRACING ?? true),
+  JWT_COOKIE_NAME: process.env.ENABLE_TRACING ?? 'AUTHENTICATION_TOKEN',
+  PORT: process.env.ENABLE_TRACING ?? "4000",
+};
+
+export default config;
--- src/services/web/config.ts
+++ src/services/web/config.ts
@@ -1,5 +1,14 @@
-export default {
-  GRAPHQL_URL: 'http://localhost:4000/graphql',
-  JWT_COOKIE_NAME: 'AUTHENTICATION_TOKEN',
-  JWT_EXISTS_APOLLO_CACHE_NAME: 'isUserAuthenticated',
+/* eslint-disable prettier/prettier */
+type configType = {
+  GRAPHQL_URL: string;
+  JWT_COOKIE_NAME: string;
+  JWT_EXISTS_APOLLO_CACHE_NAME: string;
 };
+
+const config: configType = {
+  GRAPHQL_URL: process.env.GRAPHQL_URL ?? 'http://localhost:4000/graphql',
+  JWT_COOKIE_NAME: process.env.JWT_COOKIE_NAME ?? 'AUTHENTICATION_TOKEN',
+  JWT_EXISTS_APOLLO_CACHE_NAME: process.env.JWT_EXISTS_APOLLO_CACHE_NAME ?? 'isUserAuthenticated',
+};
+
+export default config;
