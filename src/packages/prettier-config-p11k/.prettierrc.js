fd07900609329b35494e61c7c46e83462549a2e3
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-11-07 14:09:32 +0100

    chore: fix indent

--- src/packages/prettier-config-p11k/.prettierrc.js
+++ src/packages/prettier-config-p11k/.prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    arrowParens: 'always',
-    printWidth: 120,
-    semi: true,
-    singleQuote: true,
-    tabWidth: 2,
-    trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
 };
