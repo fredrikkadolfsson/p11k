8308067c621509e3a6d79f150f49a56b25298d54
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-11-07 17:36:07 +0100

    chore: change mock response values

--- src/services/account/src/endpoints/getUser.ts
+++ src/services/account/src/endpoints/getUser.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
 
 const getUser = (_: unknown, res: Response): void => {
-  res.json({ firstName: 'mockFirstName', id: 'mockId', lastName: 'mockLastName' });
+  res.json({ email: 'mockEmail', id: 'mockId' });
   return;
 };
 
