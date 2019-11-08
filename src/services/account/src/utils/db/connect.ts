88142b29bd12d798f9109a108f7c579d6fef1503
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-11-08 22:05:12 +0100

    chore: add docker-compose for development

--- src/services/account/src/utils/db/connect.ts
+++ src/services/account/src/utils/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import config from '../../config';
 
 const connectDB = async (): Promise<void> => {
-  await mongoose.connect(`mongodb://localhost:27017`, {
+  await mongoose.connect(`mongodb://mongo:27017`, {
     dbName: config.DB_NAME,
     pass: config.DB_PASSWORD,
     useCreateIndex: true,
