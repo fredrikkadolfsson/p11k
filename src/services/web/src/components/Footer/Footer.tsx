dff8f22da87670ae4496882b474b8771edb55aca
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-12-15 13:36:22 +0100

    refactor: localize footer

--- src/services/web/src/components/Footer/Footer.tsx
+++ src/services/web/src/components/Footer/Footer.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Footer as UiFooter } from '@fredrikkadolfsson/ui';
 
-const linkSections = [
-  {
-    links: [
-      { text: 'Start', url: '/' },
-      { text: 'Om oss', url: '/about' },
-      { text: 'Mitt konto', url: '/user' },
-    ],
-    title: 'Viktiga länkar',
-  },
-  {
-    links: [
-      { text: 'Logga in', url: '/login' },
-      { text: 'Registrering', url: '/signup' },
-    ],
-    title: 'Kom igång',
-  },
-];
+const Footer = (): JSX.Element => {
+  const { t } = useTranslation('common');
 
-const Footer = (): JSX.Element => <UiFooter siteName="Perfect Stack" linkSections={linkSections} />;
+  const linkSections = React.useMemo(
+    () => [
+      {
+        links: [
+          { text: t('home', 'Home'), url: '/' },
+          { text: t('about', 'About us'), url: '/about' },
+          { text: t('account', 'My account'), url: '/user' },
+        ],
+        title: t('important_links', 'Important links'),
+      },
+      {
+        links: [
+          { text: t('sign_in', 'Sign in'), url: '/login' },
+          { text: t('register', 'Register'), url: '/signup' },
+        ],
+        title: t('get_started', 'Get started'),
+      },
+    ],
+    [t],
+  );
+
+  return <UiFooter siteName={t('site_name', 'Perfect Stack')} linkSections={linkSections} />;
+};
 
 export default React.memo(Footer);
