ddde6a2176a58ae965767faadfe1b48ef0cbc2f3
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-12-15 16:27:48 +0100

    fix: add static id to enable caching

--- src/services/graphql/src/schema/resolvers/Authentication.ts
+++ src/services/graphql/src/schema/resolvers/Authentication.ts
@@ -1,5 +1,6 @@
 const Authentication = {
   Authentication: {
+    id: (): string => 'static_1',
     isAuthenticated: (token: string): boolean => Boolean(token),
     token: (token: string): string => token,
   },
--- src/services/graphql/src/schema/typeDefs/Authentication.ts
+++ src/services/graphql/src/schema/typeDefs/Authentication.ts
@@ -2,6 +2,7 @@ import { gql } from 'apollo-server-core';
 
 const Authentication = gql`
   type Authentication {
+    id: ID!
     isAuthenticated: Boolean!
     token: String!
   }
