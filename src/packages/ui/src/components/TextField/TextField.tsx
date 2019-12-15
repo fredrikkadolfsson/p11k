ac43ee19295a637303a1b7ad43a91bc5f836270b
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2019-12-15 12:21:54 +0100

    fix: add formik support to TextField

--- src/packages/ui/src/components/TextField/TextField.tsx
+++ src/packages/ui/src/components/TextField/TextField.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { default as MuiTextField, TextFieldProps } from '@material-ui/core/TextField';
+import { default as MuiTextField, TextFieldProps as MuiTextFieldProps } from '@material-ui/core/TextField';
 import styled, { StyledProps } from '../../lib';
 
 const TemproraryWrapper = styled.div`
@@ -7,10 +7,36 @@ const TemproraryWrapper = styled.div`
   margin-top: ${(props: StyledProps): string => props.theme.sizes.xxsmall};
 `;
 
-const TextField = (props: TextFieldProps): JSX.Element => (
+interface TextFieldProps {
+  id: string;
+  formik: {
+    [field: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  };
+}
+interface TextFieldPropsEmpty {
+  formik?: undefined;
+}
+
+const TextField = ({
+  id,
+  formik,
+  ...props
+}: MuiTextFieldProps & (TextFieldProps | TextFieldPropsEmpty)): JSX.Element => (
   <TemproraryWrapper>
-    {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-    <MuiTextField variant={'outlined' as any} fullWidth {...props} />
+    <MuiTextField
+      id={id}
+      fullWidth
+      variant={'outlined' as any} // eslint-disable-line @typescript-eslint/no-explicit-any
+      {...(formik &&
+        id && {
+          value: formik.values[id],
+          onChange: formik.handleChange,
+          onBlur: formik.handleBlur,
+          helperText: formik.touched[id] ? formik.errors[id] : '',
+          error: formik.touched[id] && Boolean(formik.errors[id]),
+        })}
+      {...props}
+    />
   </TemproraryWrapper>
 );
 
