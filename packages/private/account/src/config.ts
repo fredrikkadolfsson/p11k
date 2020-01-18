a841b8a8148bbd4c6f35f9d071a9c85d32b39beb
Fredrik Adolfsson <fredrikkadolfsson@users.noreply.github.com>
2020-01-18 13:37:19 +0100

    fix: update lots of packages

--- packages/private/account/src/config.ts
+++ packages/private/account/src/config.ts
@@ -1,8 +1,10 @@
+import { Algorithm } from 'jsonwebtoken';
+
 interface Config {
   DB_NAME: string;
   DB_PASSWORD: string;
   DB_USER: string;
-  JWT_ALGORITHM: string;
+  JWT_ALGORITHM: Algorithm;
   JWT_AUDIENCE: string;
   JWT_EXPIRES_IN: string;
   JWT_ISSUER: string;
@@ -14,7 +16,7 @@ const config: Config = {
   DB_NAME: process.env.DB_NAME ?? 'accounts',
   DB_PASSWORD: process.env.DB_PASSWORD ?? 'developer_password',
   DB_USER: process.env.DB_USER ?? 'developer',
-  JWT_ALGORITHM: process.env.JWT_ALGORITHM ?? 'RS256',
+  JWT_ALGORITHM: (process.env.JWT_ALGORITHM as Algorithm) ?? 'RS256',
   JWT_AUDIENCE: process.env.JWT_AUDIENCE ?? 'http://localhost:3000',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? '12h',
   JWT_ISSUER: process.env.JWT_ISSUER ?? 'Dev corp',
